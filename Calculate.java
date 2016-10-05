a382ee43a0d79f4724adffe62200318c8299cc53
Pirate <Denis@iMac-Deonisiy.local>
2016-10-05 18:17:17 +0300

    New update 05.10.16

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
     public static void main(String[] args) {
 	System.out.println("Calculate..");
+	int a = Integer.valueOf(args[0]);
+	int b = Integer.valueOf(args[1]);
+	int sum = a + b;
+	System.out.println("Sum = " + sum);
     }
 }
