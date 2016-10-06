840ad2e6db516ccb11ff7df83173c24415d9c1ef
pojarnik <pojarspam@gmail.com>
2016-10-06 19:01:38 +0300

    update a little

--- src/main/java/ru/lesson/Calculate.java
+++ src/main/java/ru/lesson/Calculate.java
@@ -6,10 +6,11 @@ package ru.lesson;
 
 public class Calculate {
     public static void main(String[] args) {
-	System.out.println("Calculate..");
-	int a = Integer.valueOf(args[0]);
-	int b = Integer.valueOf(args[1]);
-	int sum = a + b;
-	System.out.println("Sum = " + sum);
+		System.out.println("Calculate..");
+		int a = Integer.valueOf(args[0]);
+		int b = Integer.valueOf(args[1]);
+		int sum = a + b;
+		System.out.println("Sum = " + sum);
+		System.out.println("args[0].toString() = " + args[0].toString());
     }
 }
